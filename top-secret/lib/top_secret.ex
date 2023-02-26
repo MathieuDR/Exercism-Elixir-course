19d0bc963d5b23fe9321048557b4c21d0c113398
Mathieu De Raedt <mathieu@deraedt.dev>
2023-02-27 00:33:09 +0100

    Better implementation

--- top-secret/lib/top_secret.ex
+++ top-secret/lib/top_secret.ex
@@ -3,39 +3,28 @@ defmodule TopSecret do
     Code.string_to_quoted!(string)
   end
 
+  def decode_secret_message_part({op, _, args} = ast, acc) when op in [:def, :defp] do
+    {name, arity} = get_function_name_and_arity(args)
 
-  def decode_secret_message_part({_, _, [{:when, _, [a | _]} | _ ]} = ast, acc), do: {ast, accumulate(a, acc)}
-  def decode_secret_message_part({operation, _, [a | _ ]} = ast, acc) do
-    case operation do
-      :def -> {ast, accumulate(a, acc)}
-      :defp -> {ast, accumulate(a, acc)}
-      _ -> {ast, acc}
-    end
+    str = String.slice(name, 0, arity)
+    {ast, [str | acc]}
   end
 
   def decode_secret_message_part(ast, acc), do: {ast, acc}
 
-  defp accumulate(node, acc) do
-    [name, params] = decode_function(node)
-    append_string(name, acc, params)
-  end
-
-  defp decode_function({name, _, params}), do: [name, params]
-
-  defp append_string(_, acc, nil), do: ["" | acc]
-
-  defp append_string(atom, acc, params) do
-    str = Atom.to_string(atom)
-    |> String.slice(0, Enum.count(params))
-
-    [str | acc]
+  defp get_function_name_and_arity([node | _]) do
+    case node do
+      {:when, _, args} -> get_function_name_and_arity(args)
+      {name, _, nil} -> {Atom.to_string(name), 0}
+      {name, _, params} -> {Atom.to_string(name), Enum.count(params)}
+      _ -> raise "uh oh"
+    end
   end
 
   def decode_secret_message(string) do
     to_ast(string)
-    |> Macro.prewalk([] , &decode_secret_message_part/2)
+    |> Macro.prewalk([], &decode_secret_message_part/2)
     |> elem(1)
     |> Enum.reduce("", &<>/2)
-
   end
 end
