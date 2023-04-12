47190c13b2ac4d52839e26667d1c4b61bef76d8f
Mathieu De Raedt <mathieu@deraedt.dev>
2023-04-12 10:29:11 +0200

    upgrades

--- two-fer/lib/two_fer.ex
+++ two-fer/lib/two_fer.ex
@@ -1,13 +1,11 @@
 defmodule TwoFer do
+  @moduledoc """
+  This is the TwoFer module to give out cookies.
+  """
+
   @doc """
   Two-fer or 2-fer is short for two for one. One for you and one for me.
   """
-
-  @spec two_fer() :: String.t()
-  def two_fer(), do: "One for you, one for me."
-
   @spec two_fer(String.t()) :: String.t()
-  def two_fer(name) when is_binary(name), do: "One for #{name}, one for me."
-
-  def two_fer(_), do: raise(FunctionClauseError)
+  def two_fer(name \\ "you") when is_binary(name), do: "One for #{name}, one for me."
 end
